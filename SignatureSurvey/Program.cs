2b5fa59a20eea9f93c05b4f026abc04cd4db7a8a
Alexander Groß <agross@therightstuff.de>
2012-09-23 16:44:54 +0200

    Initial commit

---
+++ SignatureSurvey/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace SignatureSurvey
+{
+  class Program
+  {
+    enum ExitCode
+    {
+      Success = 0,
+      FileNotFound = 1,
+      GenericError = 2
+    }
+
+    static readonly char[] Chars = { '{', '}', ';' };
+
+    static void Main(string[] args)
+    {
+      try
+      {
+        var path = GetPathToFile(args);
+        Inspect(path);
+
+        Environment.Exit((int) ExitCode.Success);
+      }
+      catch (Exception exception)
+      {
+        Console.WriteLine(exception);
+        Environment.Exit((int) ExitCode.GenericError);
+      }
+    }
+
+    static string GetPathToFile(string[] args)
+    {
+      var path = args[0];
+      if (!File.Exists(path))
+      {
+        Console.WriteLine("File '{0}' does not exist.", path);
+        Environment.Exit((int) ExitCode.FileNotFound);
+      }
+      return path;
+    }
+
+    static void Inspect(string path)
+    {
+      using (var stream = File.OpenRead(path))
+      {
+        var pattern = new StringBuilder();
+        var lines = 1;
+
+        for (var i = 0; i < stream.Length; i++)
+        {
+          var c = (char) stream.ReadByte();
+          if (Chars.Contains(c))
+          {
+            pattern.Append(c);
+          }
+          if (c == Environment.NewLine.Last())
+          {
+            lines = lines + 1;
+          }
+        }
+
+        Console.WriteLine("{0} {1} {2}", lines, path, pattern);
+      }
+    }
+  }
+}
